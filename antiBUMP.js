f96a003544233b8d077aa1d5b21eca53b5b0823b
lenchik-lox <55487043+lenchik-lox@users.noreply.github.com>
2020-04-30 23:54:04 +0500

    antiBUMP ver 1.2

--- antiBUMP.js
+++ antiBUMP.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         antiBUMP
 // @namespace    koq
-// @version      1.1
+// @version      1.2
 // @description  BUMP OUT OF HERE. removes all messages with "BUMP" an 2ch.
-// @author       dik & dok
+// @author       dik&dok
 // @match        *://2ch.hk/*/res/*
 // @match        *://2ch.*/*
 // @grant        none
@@ -11,7 +11,8 @@
 // @updateUrl    https://raw.githubusercontent.com/lenchik-lox/antiBUMP/master/antiBUMP.js
 // ==/UserScript==
 
-(function() {
+
+    'use strict';
     function getCookie(name) {
         var matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
         return matches ? decodeURIComponent(matches[1]) : undefined;
@@ -50,27 +51,30 @@
         var them_cum = document.getElementsByClassName('post__message');
         var post = document.querySelectorAll('.post__anon');
         for (var lublu_karandashi = 0; lublu_karandashi<them_cum.length; lublu_karandashi++) {
-            if (them_cum[lublu_karandashi].innerText.toLowerCase() == ("бамп" || "bump" || "/bump!?/" || "bamp" || "бабамп" || "бымп" || "бумп")){
-                them_cum[lublu_karandashi].style.display = 'none';
-                post[lublu_karandashi].innerText = "бамп";
+            //if (them_cum[lublu_karandashi].innerText.toLowerCase() == ("бамп" || "bump" || "/bump!?/" || "bamp" || "бабамп" || "бымп" || "бумп")){
+            var jalil = ["бамп","бап","бам","бапм","bamp","bump","b*mp","бабамп","бумп","бамп!","бамп?","бымп","бомп","бамж","бвмп","bmpp","бюмп"/*,"","","","","","","","","",""*/];
+            for (var xXx_nagibator666_xXx = 0; xXx_nagibator666_xXx<jalil.length;xXx_nagibator666_xXx++) {
+                if (them_cum[lublu_karandashi].innerText.toLowerCase().indexOf(jalil[xXx_nagibator666_xXx])>=0 && (them_cum[lublu_karandashi].innerText.length<5||them_cum[lublu_karandashi].innerText.toLowerCase().indexOf(">>")>=0)) {
+                    them_cum[lublu_karandashi].style.display = 'none';
+                    post[lublu_karandashi].innerText = "бамп";
+                }
             }
+            //}
         }
-        d0k="";d2k = "left:-14px;";d3k="background:#FF972F;";d1k = "text-decoration:line-through";
+        s.style+="left:30px;"; m.style+="background:orange;"; d2k = "left:-28px;"; d3k = "background:#9D9D9D;"; d1k = "text-decoration:none";
+        //d0k="";d2k = "left:-14px;";d3k="background:#FF972F;";d1k = "text-decoration:line-through";
     }
     else if (dook=="false") {
-        //da
-        s.style+="left:30px;";
-        m.style+="background:orange;";
-        d2k = "left:-28px;";
-        d3k = "background:#9D9D9D;";
-        d1k = "text-decoration:none";
+        d0k="";d2k = "left:-14px;";d3k="background:#FF972F;";d1k = "text-decoration:line-through";
+        //s.style+="left:30px;"; m.style+="background:orange;"; d2k = "left:-28px;"; d3k = "background:#9D9D9D;"; d1k = "text-decoration:none";
     }
     //m.className = s.className = "koqq";
     t.innerHTML = "bump";
     t.style.cssText += ";transition:.2s ease-in-out;left:0px;position:relative;"+d1k;
     m.style.cssText += ";transition:.2s ease-in-out;border-radius:10px;vertical-align:middle;width:32px;height:16px;; position:relative;display:inline-block; padding:2px; cursor:pointer;"+d3k;
     s.style.cssText += ";transition:.2s ease-in-out;border-radius:100%;vertical-align:middle;width:10px;height:10px;background:#EEEEEE;position:relative;display:inline-block;margin:-1px;cursor:pointer;"+d2k;
-    s.onclick = m.onclick = function() {
+    s.onclick = m.onclick = kak
+        function kak() {
         if (getCookie("bb")=="true") {
             s.style += ";transition:.2s ease-in-out;border-radius:100%;vertical-align:middle;width:10px;height:10px;background:#EEEEEE;position:relative;display:inline-block;margin:-1px;cursor:pointer;left:-14px;";
             m.style += ";transition:.2s ease-in-out;border-radius:10px;vertical-align:middle;width:32px;height:16px;; position:relative;display:inline-block; padding:2px; cursor:pointer;background:#FF972F;";
@@ -86,11 +90,11 @@
         //setTimeout(function() {location.reload()},200);
     }
     var koqs = document.querySelectorAll('span.adminbar__cat')[1];
-    
+
     koqs.appendChild(m);
     koqs.appendChild(s);
     koqs.appendChild(t);
-    
+
     for (var ksux = 0;ksux<document.querySelectorAll('.spoiler').length;ksux++) {
         var d5k = document.querySelectorAll('.spoiler')[ksux];
         d5k.style += ";cursor:pointer;";
@@ -99,4 +103,3 @@
     function kok() {
         this.style += ";background:none;color:#333333;";
     }
-})
